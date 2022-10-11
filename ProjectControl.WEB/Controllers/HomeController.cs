195c3ebde13268287fa1e9fc268d0c6e28316461
ChamzyK <chamzykyrgys@mail.ru>
2022-10-11 15:26:16 +0700

    Implemented home controller.

--- ProjectControl.WEB/Controllers/HomeController.cs
+++ ProjectControl.WEB/Controllers/HomeController.cs
@@ -4,9 +4,10 @@ namespace ProjectControl.WEB.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<IActionResult> Index()
-        {
-            return await Task.Run(() => View());
-        }
+        public async Task<IActionResult> Index() => await Task.Run(() => View());
+
+        public async Task<IActionResult> Projects() => await Task.Run(() => View());
+
+        public async Task<IActionResult> Employees() => await Task.Run(() => View());
     }
 }
