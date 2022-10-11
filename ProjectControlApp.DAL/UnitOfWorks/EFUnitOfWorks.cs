f587754ecb0ce987ba6938684fa8c89f4a3846ba
ChamzyK <chamzykyrgys@mail.ru>
2022-10-11 11:13:52 +0700

    Added ef unit of work.

--- ProjectControlApp.DAL/UnitOfWorks/EFUnitOfWorks.cs
+++ ProjectControlApp.DAL/UnitOfWorks/EFUnitOfWorks.cs
@@ -1,6 +1,50 @@
-﻿namespace ProjectControlApp.DAL.UnitOfWorks;
+﻿using ProjectControl.Domain.Entities;
+using ProjectControl.Domain.Interfaces.Repositories;
+using ProjectControl.Domain.Interfaces.UnitOfWorks;
+using ProjectControlApp.DAL.EF;
+using ProjectControlApp.DAL.Repositories.Generic;
 
-public class EFUnitOfWorks
+namespace ProjectControlApp.DAL.UnitOfWorks;
+
+public class EFUnitOfWorks : IUnitOfWork
 {
+    private readonly ProjectControlAppContext _context;
+    private bool _disposed;
 
+    public Dictionary<Type, object> Repositories { get; set; }
+
+    public EFUnitOfWorks(ProjectControlAppContext context)
+    {
+        _context = context;
+
+        //TODO:get objects from outside (maybe injection?)
+        Repositories = new Dictionary<Type, object>
+        {
+            { typeof(Project), new EFGenericRepository<Project>(_context) },
+            { typeof(Employee), new EFGenericRepository<Employee>(_context) },
+            { typeof(Participation), new EFGenericRepository<Participation>(_context) }
+        };
+    }
+
+    public IGenericRepository<TEntity> GetGenericRepository<TEntity>()
+        where TEntity : class
+    {
+        return (IGenericRepository<TEntity>)Repositories[typeof(TEntity)];
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+
+    void IDisposable.Dispose()
+    {
+        if (_disposed || _context == null)
+        {
+            return;
+        }
+        _disposed = true;
+        _context.Dispose();
+        GC.SuppressFinalize(this);
+    }
 }
