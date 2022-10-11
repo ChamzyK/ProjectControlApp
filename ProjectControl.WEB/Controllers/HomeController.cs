455f9d35881975a30c6e7673c35955c265d5a504
ChamzyK <chamzykyrgys@mail.ru>
2022-10-11 12:09:17 +0700

    Added swagger.

---
+++ ProjectControl.WEB/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectControl.WEB.Controllers
+{
+    public class HomeController
+    {
+
+    }
+}
